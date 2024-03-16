7885012cf5187d02ab66ccf7d2d4cca20c1e733d
vinayak <vinayakshukla2000@gmail.com>
2024-03-17 01:08:33 +0530

    showing quizzes details for admin

--- src/app/pages/admin/view-quizzes/view-quizzes.component.ts
+++ src/app/pages/admin/view-quizzes/view-quizzes.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QuizService } from '../../../services/quiz.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-quizzes',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-quizzes.component.scss']
 })
 export class ViewQuizzesComponent {
+  quizzes=[{
+    qId: 23,
+    title: 'Basic Java Quiz',
+    description: '',
+    maxMarks: 50,
+    numberOfQuestions: 20, 
+    active: '',
+    category:{
+      title:"programming"},
+  },
+  {
+    qId: 24,
+    title: ' Java Quiz',
+    description: '',
+    maxMarks: 50,
+    numberOfQuestions: 20, 
+    active: '',
+    category:{
+      title:"programming"}
+  }
 
+]
+constructor(private _quiz: QuizService){}
+ngOnInit():void{
+  this._quiz.quizzes().subscribe({
+    next: (data:any)=>{
+    this.quizzes=data;
+    },
+    error: (error:any)=>{
+      Swal.fire('Error!', "Error in loading data!","error" );
+    }
+   })
+  
+}
+ 
 }
